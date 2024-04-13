4417051a0bd7e1893ca4000a4d498962e85e8db9
Abdullah Hasan Sajjad Rafi <rafiabdullahhasansajjad@gmail.com>
2024-04-13 20:03:16 +0600

    get movie by imdbid implemented

--- movieAPI/movieAPI/src/main/java/com/rafi/movieAPI/MovieController.java
+++ movieAPI/movieAPI/src/main/java/com/rafi/movieAPI/MovieController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/movies")
@@ -19,6 +21,11 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> getMovies() {
         return new ResponseEntity<List<Movie>>(service.findAllMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(service.findMovieByImdbID(imdbId), HttpStatus.OK);
     }
 
 }
